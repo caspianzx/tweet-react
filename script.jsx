390d877cf42441ee3f60ca4f64ee36ae1a83ff44
caspianzx <caspianzx@gmail.com>
2019-08-27 17:42:25 +0800

    updated some css

--- script.jsx
+++ script.jsx
@@ -1,10 +1,11 @@
 class TweetCard extends React.Component {
     render() {
         return (
-            <div>
-            <div>{this.props.screen_name}</div>
-           <div><img src={this.props.img}/></div>
-            <div>{this.props.text}</div>
+            <div className ="col-6 offset-3">
+                <h3>{this.props.screen_name}</h3>
+                <div><img src={this.props.img}/></div>
+                <div>Tweet: {this.props.text}</div>
+                <div> number of followers: {this.props.followers_count}</div>
             </div>
         );
     }
@@ -13,11 +14,11 @@ class TweetCard extends React.Component {
 class Tweets extends React.Component {
     render() {
         let eachTweet = this.props.items.map( item => {
-                              return <TweetCard screen_name ={item.user.screen_name} img = {item.user.profile_image_url} text = {item.text} ></TweetCard>
+                              return <TweetCard screen_name ={item.user.screen_name} img = {item.user.profile_image_url} text = {item.text} followers_count={item.user.followers_count} ></TweetCard>
                                      ;
                             });
         return (
-          <div>
+          <div className ="row">
             {eachTweet}
           </div>
         );
