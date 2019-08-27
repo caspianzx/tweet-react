59664e889295296871669c28221575ec5e8497a1
caspianzx <caspianzx@gmail.com>
2019-08-27 17:23:51 +0800

    completed mvp. no css though

--- script.jsx
+++ script.jsx
@@ -1,7 +1,32 @@
-var monkey = "hello";
+class TweetCard extends React.Component {
+    render() {
+        return (
+            <div>
+            <div>{this.props.screen_name}</div>
+           <div><img src={this.props.img}/></div>
+            <div>{this.props.text}</div>
+            </div>
+        );
+    }
+}
+
+class Tweets extends React.Component {
+    render() {
+        let eachTweet = this.props.items.map( item => {
+                              return <TweetCard screen_name ={item.user.screen_name} img = {item.user.profile_image_url} text = {item.text} ></TweetCard>
+                                     ;
+                            });
+        return (
+          <div>
+            {eachTweet}
+          </div>
+        );
+    }
+}
+
+
+
 ReactDOM.render(
-    <div>
-      {monkey}
-    </div>,
-    document.getElementById('root')
+ <Tweets items = {tweets}/>,
+    document.getElementById('allTweet')
 );
